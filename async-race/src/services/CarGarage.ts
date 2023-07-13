ee039eab6d3bd90421ca2dcc472cdcd6d25a5d55
Vadim Shidlov <mat23vadim@mail.ru>
2023-07-13 14:34:30 +0300

    feat: create service CarGarage

--- async-race/src/services/CarGarage.ts
+++ async-race/src/services/CarGarage.ts
@@ -3,27 +3,74 @@ export type StartEngineType = {
   distance: string;
 };
 
-export type StartDriveType = {
-  success: boolean;
+export type GetCarsType = {
+  name: string;
+  color: string;
+  id: boolean;
+};
+
+export type UpdateCarType = {
+  name: string;
+  color: string;
+  id: boolean;
 };
 
 export class CarGarage {
-  private readonly engineUrl = '/garage';
+  private readonly garageUrl = 'http://127.0.0.1:3000/garage/';
 
-  public async getCars(id: number): Promise<StartEngineType> {
-    const response = await fetch(`http://127.0.0.1:3000${this.engineUrl}/id=${id}&status=started`);
+  public async getCars(page?: number, limit?: number): Promise<GetCarsType[]> {
+    let url: string;
+    if (page && limit) {
+      url = `${this.garageUrl}_page=${page}&_limit=${limit}`;
+    } else {
+      url = `http://127.0.0.1:3000${this.garageUrl}`;
+    }
+
+    const response = await fetch(url);
 
     return response.json();
   }
 
-  public async stopEgine(id: number): Promise<StartEngineType> {
-    const response = await fetch(`http://127.0.0.1:3000${this.engineUrl}/id=${id}&status=stopped`);
+  public async getCar(id: number): Promise<GetCarsType> {
+    const url = `${this.garageUrl}${id}`;
+    const response = await fetch(url);
 
     return response.json();
   }
 
-  public async driveEgine(id: number): Promise<StartEngineType> {
-    const response = await fetch(`http://127.0.0.1:3000${this.engineUrl}/id=${id}&status=drive`);
+  public async createCar(name: string, color: string): Promise<GetCarsType> {
+    const response = await fetch(this.garageUrl, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({ name, color }),
+    });
+
+    return response.json();
+  }
+
+  public async deleteCar(id: number): Promise<object> {
+    const url = `${this.garageUrl}${id}`;
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'DELETE',
+    });
+
+    return response.json();
+  }
+
+  public async updateCar(id: number, name: string, color: string): Promise<UpdateCarType> {
+    const url = `${this.garageUrl}${id}`;
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PUT',
+      body: JSON.stringify({ name, color }),
+    });
 
     return response.json();
   }
